d3c681069224b8c51b038c54a68f1b351a5c6a7d
Fateme <fsamandarnejad2001@gmail.com>
2024-09-05 18:59:11 +0330

    Add api app

--- {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/urls.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.views.generic import RedirectView
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
@@ -25,4 +25,5 @@ urlpatterns = [
     path('schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     path('admin/', admin.site.urls),
+    path('api/', include(('apps.api.urls', 'api'))),
 ]
